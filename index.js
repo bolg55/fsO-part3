d5948aa936ffaf15b34c185fd4224bcd22757f74
bolg55 <bolger.kellen@gmail.com>
2021-09-25 01:48:14 -0400

    added database w.  mongoose and mongodb

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const app = express();
-const PORT = process.env.PORT || 3001;
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+const PORT = process.env.PORT || 3001
 
 let data = [
   {
@@ -25,74 +25,74 @@ let data = [
     name: 'Mary Poppendieck',
     number: '39-23-6423122',
   },
-];
+]
 // Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.static('build'));
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
 
 // Format morgan token to return POST req
-morgan.token('reqParams', (req) => JSON.stringify(req.body));
+morgan.token('reqParams', (req) => JSON.stringify(req.body))
 const morganFormat =
-  ':method :url :status :res[content-length] - :response-time ms :reqParams';
-app.use(morgan(morganFormat));
+  ':method :url :status :res[content-length] - :response-time ms :reqParams'
+app.use(morgan(morganFormat))
 
 // Utility functions
 const generateId = () => {
-  const maxId = data.length > 0 ? Math.max(...data.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+  const maxId = data.length > 0 ? Math.max(...data.map((n) => n.id)) : 0
+  return maxId + 1
+}
 
 // Root route
 app.get('/', (req, res) => {
-  res.send('<h1>working!</h1>');
-});
+  res.send('<h1>working!</h1>')
+})
 
 // Info route
 app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${data.length} people</p>
-    <p>${new Date()}</p>`);
-});
+    <p>${new Date()}</p>`)
+})
 
 // GET ALL data from API as JSON object
 app.get('/api/persons', (req, res) => {
-  res.json(data);
-});
+  res.json(data)
+})
 
 // GET ONE data by ID from API as JSON object. Return 404 if doesn't exist
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = data.find((person) => person.id === id);
+  const id = Number(req.params.id)
+  const person = data.find((person) => person.id === id)
 
   if (person) {
-    res.json(person);
+    res.json(person)
   } else {
-    res.status(404).end();
+    res.status(404).end()
   }
-});
+})
 
 // DELETE by ID from API.
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  data = data.filter((person) => person.id !== id);
+  const id = Number(req.params.id)
+  data = data.filter((person) => person.id !== id)
 
-  res.status(204).end();
-});
+  res.status(204).end()
+})
 
 // ADD NEW user and phone.
 app.post('/api/persons', (req, res) => {
-  const body = req.body;
+  const body = req.body
   // Check if name or number are empty. If true, send error
   if (!body.name || !body.number) {
     return res.status(400).json({
       error: `${!body.name ? 'name' : 'number'} is missing and required.`,
-    });
+    })
   }
   // Check if name already exists in phonebook. If exists >> send error
   if (data.find((n) => n.name === body.name)) {
     return res
       .status(400)
-      .json({ error: `${body.name} already exists in phonebook` });
+      .json({ error: `${body.name} already exists in phonebook` })
   }
   // If name doesn't exist && name / number not empty
   // create new person Object and add to copy of existing data array.
@@ -100,14 +100,14 @@ app.post('/api/persons', (req, res) => {
     id: generateId(),
     name: body.name,
     number: body.number,
-  };
+  }
 
-  data = data.concat(person);
+  data = data.concat(person)
 
-  res.json(person);
-});
+  res.json(person)
+})
 
 // Express server
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-});
+  console.log(`Server running on port: ${PORT}`)
+})
